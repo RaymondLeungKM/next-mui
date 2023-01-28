9699b8a5e6088cbc1712550af671188fc358b7fd
RaymondLeungKM <lkmraymond@hotmail.com>
2023-01-29 00:05:12 +0800

    - Sync Macbook changes

--- components/tesla.js
+++ components/tesla.js
@@ -12,12 +12,28 @@ function Tesla(props) {
   console.log(materials);
 
   // const carModel = Object.entries(nodes).map((t, k) => {
-  //   if (t[1].material) {
+  //   if (t[1].type == "Mesh") {
   //     return <mesh key={k} geometry={t[1].geometry} material={t[1].material} scale={t[1].scale}></mesh>;
   //   }
   // });
 
-  const carModel = nodes.Sketchfab_model.children[0].map()
+  const carModel = Object.entries(nodes).map((t, k) => {
+    if (t[1].type == "Object3D" && t[1].children.length == 1) {
+      console.log(t[1])
+      return (
+        <mesh
+          key={k}
+          geometry={t[1].children[0].geometry}
+          material={t[1].children[0].material}
+          scale={t[1].scale}
+        ></mesh>
+      );
+    }
+  });
+
+  // const carModel = nodes.Sketchfab_model.children[0].children.map((child)=>{
+  //   return <mesh key={child.children[0].uuid} geometry={child.children[0].geometry} material={child.children[0].material} scale={child.children[0].scale}></mesh>;
+  // })
 
   return (
     // <>
